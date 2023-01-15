c4854eee2f85b3971df7fdd215fab84472fdfccf
Maniek <s21544@pjwstk.edu.pl>
2023-01-15 10:35:59 +0100

    base zad1

---
+++ zad1/src/main/java/Note.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Note {
+    public List getAllNotesOf(String name){
+        return List.of("Note1", "Note2");
+    }
+
+    public int getNote(){
+        return 1;
+    }
+}
